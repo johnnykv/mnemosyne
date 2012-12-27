6ba3c9c7324241d8b09779e07f9c1d748d578caf
Johnny Vestergaard <jkv@unixcluster.dk>
2012-12-28 00:44:38 +0100

    Preliminary kippo support

---
+++ Test/normalizers/kippo_test.py
@@ -0,0 +1,59 @@
+# Copyright (C) 2012 Johnny Vestergaard <jkv@unixcluster.dk>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+import unittest
+from normalizers import kippo_events
+from datetime import datetime
+
+
+class KippoTests(unittest.TestCase):
+    def test_session_without_ttylog(self):
+        """
+        Test if a valid kippo json message get parsed as expected.
+        """
+        #kippo hpfeed modules does not provide an timestamp, we must use the one
+        #provided by mnemosyne
+
+        input_submission_time = datetime(2012, 12, 14, 12, 22, 51)
+        input_string = """{\"peerIP\": \"223.5.23.53\", \"loggedin\": null, \"ttylog\": null, \"hostIP\": \"192.168.6.211\", \"peerPort\": 36868, \"version\": \"SSH-2.0-libssh-0.1\", \"hostPort\": 2222, \"credentials\": [[\"root\", \"123muie123\"]]}"""
+        session_ssh = {'version': 'SSH-2.0-libssh-0.1'}
+
+        auth_attempts = [{'login': 'root',
+                          'password': 'YOT#x$ROSa@+'}]
+
+        session = {
+            'timestamp': datetime(2012, 12, 14, 12, 22, 51),
+            'source_ip': '1.2.3.4',
+            'source_port': 36228,
+            'destination_port': 2222,
+            'honeypot': 'Kippo',
+            'protocol': 'ssh',
+            'session_ssh': session_ssh,
+            'auth_attempts': auth_attempts
+        }
+
+        expected_output = [{'session': session}, ]
+
+        sut = kippo_events.KippoEvents()
+        result = sut.normalize(input_string, 'kippo.sessions', input_submission_time)
+
+        #test if we got the correct amount of root (document) items
+        self.assertEqual(len(expected_output), len(result))
+
+        self.assertItemsEqual(expected_output[0]['session'], result[0]['session'])
+        self.assertItemsEqual(expected_output[0]['session']['session_ssh'], result[0]['session']['session_ssh'])
+        self.assertItemsEqual(expected_output[0]['session']['auth_attempts'], result[0]['session']['auth_attempts'])
---
+++ normalizers/kippo_events.py
@@ -0,0 +1,47 @@
+# Copyright (C) 2012 Johnny Vestergaard <jkv@unixcluster.dk>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+from basenormalizer import BaseNormalizer
+import json
+
+
+class KippoEvents(BaseNormalizer):
+    channels = ('kippo.sessions',)
+
+    def normalize(self, data, channel, submission_timestamp):
+        o_data = json.loads(data)
+
+        session = {
+            'timestamp': submission_timestamp,
+            'source_ip': o_data['peerIP'],
+            'source_port': o_data['peerPort'],
+            'destination_port': o_data['hostPort'],
+            'honeypot': 'Kippo',
+            'protocol': 'ssh',
+            'session_ssh': {'version': o_data['version']}
+        }
+
+        if len(o_data['credentials']) > 0:
+            auth_attempts = []
+            for cred in o_data['credentials']:
+                auth_attempts.append({'login': cred[0],
+                                      'password': cred[1]})
+            session['auth_attempts'] = auth_attempts
+
+        relations = [{'session': session}, ]
+
+        return relations
