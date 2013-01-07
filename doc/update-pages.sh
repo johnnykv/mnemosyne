d9ae6a854863770345ff63c99e094536b0127d3b
Johnny Vestergaard <jkv@unixcluster.dk>
2013-01-07 22:22:12 +0100

    script to update docs site

---
+++ doc/update-pages.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#Script to update gh-pages
+
+set -e
+
+TMP_REPO=`mktemp -d -t mnemo-site`
+TMP_HTML=`mktemp -d -t mnemo-site`
+
+make html
+cp -R _build/html/* $TMP_HTML
+
+git clone git@github.com:johnnykv/mnemosyne.git $TMP_REPO
+cd $TMP_REPO
+git checkout gh-pages
+git symbolic-ref HEAD refs/heads/gh-pages
+rm .git/index
+git clean -fdx
+cp -R $TMP_HTML/* $TMP_REPO
+git add .
+git commit -a -m "Updated docs"
+git push origin gh-pages
+rm -rf $TMP_REPO
+rm -rf $TMP_HTML
