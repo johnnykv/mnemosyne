9baf4375d6e06df35ad6c6085ba0868f5a8f65b7
Johnny Vestergaard <jkv@unixcluster.dk>
2013-01-30 00:03:23 +0100

    initial middleware work

--- webapi/mnemowebapi.py
+++ webapi/mnemowebapi.py
@@ -17,8 +17,10 @@
 
 import bottle
 import shared_state
-from bottle import run, static_file, get, install, Bottle, mount
+from bottle import run, install, mount
 from bottle.ext import mongo
+from beaker.middleware import SessionMiddleware
+
 
 class MnemoWebAPI():
     """Exposes raw and normalized data from hpfeeds through a RESTful api"""
@@ -26,13 +28,28 @@ class MnemoWebAPI():
     def __init__(self, datebase_name, static_file_path=None):
         shared_state.static_dir = static_file_path
         shared_state.plug = bottle.ext.mongo.MongoPlugin(uri="localhost", db=datebase_name, json_mongo=True)
+        #install mongo plugin for root app
         install(shared_state.plug)
+
+        #load devel api
         from webapi.api import app as develapp
         mount('/api/', develapp.app)
 
-        #servestaic must be imported AFTER mounts.
+        #must be imported AFTER mounts.
         if shared_state.static_dir != None:
             import servestatic
 
+        #wrap root app in beaker middleware
+        session_opts = {
+            'session.type': 'file',
+            'session.cookie_expires': 300,
+            'session.data_dir': './data',
+            'session.auto': True,
+            #set secure attribute on cookie
+            'session.secure': True
+            }
+        self.app = SessionMiddleware(bottle.app(), session_opts)
+
+
     def start_listening(self, host, port):
-        run(host=host, port=port, debug=True, server='paste', quiet=True)
+        run(app=self.app, host=host, port=port, debug=True, server='paste', quiet=True)
